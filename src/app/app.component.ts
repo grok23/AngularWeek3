6145df0cd24d570491b39e59b2c5d3a80db7db9e
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-07 11:36:49 +0000

    further work done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MovieService} from  './movie.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Week3Injection';
+  movies=[];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.movies = this.movieService.getMovies();
+    console.log(this.movies);
+  }
 }
+
+
