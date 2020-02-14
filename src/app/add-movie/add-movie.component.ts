e9a3515d0533f9a40b643daacaa72525b65f4f6c
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-14 16:36:17 +0000

    testing why uploads aren't going to gihub

--- src/app/add-movie/add-movie.component.ts
+++ src/app/add-movie/add-movie.component.ts
@@ -9,7 +9,7 @@ import { MovieService } from '../movie.service';
 
 export class AddMovieComponent implements OnInit {
   ngOnInit(): void {
-    //throw new Error("Method not implemented.");
+    //throw new Error("Method not implemented.");  //this is commented out to prevent error messages in the console
   }
 
   constructor(private movieService: MovieService) { }
