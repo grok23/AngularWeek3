de35776f1ee1aad41f717f842ed02a151bccab89
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-14 11:46:10 +0000

    finished and errors in html corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieComponent,
+    ListMoviesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/list-movies/list-movies.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service'
+
+@Component({
+  selector: 'app-list-movies',
+  templateUrl: './list-movies.component.html',
+  styleUrls: ['./list-movies.component.css']
+})
+export class ListMoviesComponent implements OnInit {
+
+  movies=[];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.movies = this.movieService.getMovies();
+    console.log(this.movies); 
+  }
+}
