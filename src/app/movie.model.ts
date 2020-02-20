8eb2a517fe5a32abe4c06665e31042649836d5cc
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-20 11:12:06 +0000

    added Movie class

---
+++ src/app/movie.model.ts
@@ -0,0 +1,9 @@
+/*will provide us with a "Movie" object*/
+
+export class Movie{
+    constructor(
+        public title:string,
+        public year:string,
+        public director:string,) {
+        }
+}
