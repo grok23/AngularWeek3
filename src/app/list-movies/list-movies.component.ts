44cbe73faf132d40ca5bea99e06e69b86f53a27a
grok23 <46896171+grok23@users.noreply.github.com>
2020-02-20 12:08:32 +0000

    added output and event emitter for movie selection

--- src/app/list-movies/list-movies.component.ts
+++ src/app/list-movies/list-movies.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MovieService } from '../movie.service'
+import { Movie } from '../movie.model';
 
 @Component({
   selector: 'app-list-movies',
   templateUrl: './list-movies.component.html',
   styleUrls: ['./list-movies.component.css']
 })
+
 export class ListMoviesComponent implements OnInit {
 
   movies=[];
+  private currentMovie:Movie;
 
-  constructor(private movieService: MovieService) {}
+  @Output() onSelectedMovie: EventEmitter<Movie>;
+
+  constructor(private movieService: MovieService) {
+    this.onSelectedMovie=new EventEmitter();
+  }
 
   ngOnInit() {
     this.movies = this.movieService.getMovies();
     console.log(this.movies); 
+  }
+  movieSelected(movie:Movie):void{
+    console.log ('movie selected was ', movie);
+  }
+
+  selectMovie(myMovie:Movie):void {
+    console.log(myMovie);
+    this.currentMovie=myMovie;
+    this.onSelectedMovie.emit(myMovie);
+  }
+
+  isSelected(movie:Movie): boolean {
+    if (!movie || !this.currentMovie) {
+      return false;
+    }
+    return movie.title === this.currentMovie.title;
   }
 }
